da0612fb0c3362dc0c9d5131231353f26a832ff7
crxs14 <59511107+mahdizaabi@users.noreply.github.com>
2021-06-06 20:48:01 +0200

    Delete mkdir.js

--- mkdir.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs")
-
-fs.mkdir("/home/vagrant/testDir", function(err) {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log("New directory successfully created.")
-  }
-})
