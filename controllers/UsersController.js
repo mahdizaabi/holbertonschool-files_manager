1bd6c54ef15e91f5dff77a3d57062e7fc0b709ae
mahdi <mahdi.zaabi@gmail.com>
2021-06-06 23:34:46 +0000

    add

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,32 +3,34 @@ import DBClient from '../utils/db';
 import Auth from '../utils/Auth';
 
 class UsersController {
+  /**
+   * Check the request body for user's 'email' and 'password'. If user already exists
+   * should response with 400, otherwise create a new user in the DB using 'dbClient'.
+   *
+   * @param  {Object} request  - HTTP request object
+   * @param  {Object} response - HTTP response object
+   * @return {Object} http response
+   */
   static async postNew(req, res) {
     const { email, password } = req.body;
     if (!email) {
-      res.status(400).send({ error: 'Missing email' });
-      return;
+      return res.status(400).send({ error: 'Missing email' });
     }
     if (!password) {
-      res.status(400).send({ error: 'Missing password' });
-      return;
+      return res.status(400).send({ error: 'Missing password' });
     }
-    try {
-      const checkedEmail = await DBClient.checkEmail(email);
-      if (checkedEmail) {
-        res.status(400).send({ error: 'Already exist' });
-        return;
-      }
-    } catch (e) {
-      res.status(400).send({ error: 'Already exist' });
+
+    const checkedEmail = await DBClient.checkEmail(email);
+    if (checkedEmail) {
+      return res.status(400).send({ error: 'Already exist' });
     }
 
     try {
-      const ops = await DBClient.setNewUser({ email, password: sha1(password) });
-      res.status(201).send(JSON.stringify({ id: ops.insertedId, email }));
-      return;
+      const hashedPassword = sha1(password);
+      const ops = await DBClient.setNewUser({ email, password: hashedPassword });
+      return res.status(201).send(JSON.stringify({ id: ops.insertedId, email }));
     } catch (err) {
-      res.status(500).send({ error: 'internal Error' });
+      return res.status(500).send({ error: 'internal Error' });
     }
   }
 
