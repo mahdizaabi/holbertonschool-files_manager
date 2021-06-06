de7155b1802accfb69c6058f9e467ad6100da33c
mahdi <mahdi.zaabi@gmail.com>
2021-06-06 23:48:06 +0000

    edit response format

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,35 +3,31 @@ import DBClient from '../utils/db';
 import Auth from '../utils/Auth';
 
 class UsersController {
-  /**
-   * Check the request body for user's 'email' and 'password'. If user already exists
-   * should response with 400, otherwise create a new user in the DB using 'dbClient'.
+  static async postNew(req, res) {
+    /**
+   * parse password and email from the request object and check if the email is already on the db
+   * create a new User and return 201, 400 otherwise.
    *
    * @param  {Object} request  - HTTP request object
    * @param  {Object} response - HTTP response object
    * @return {Object} http response
    */
-  static async postNew(req, res) {
     const { email, password } = req.body;
     if (!email) {
-      return res.status(400).send({ error: 'Missing email' });
+      return res.status(400).json({ error: 'Missing email' });
     }
     if (!password) {
-      return res.status(400).send({ error: 'Missing password' });
+      return res.status(400).json({ error: 'Missing password' });
     }
 
-    const checkedEmail = await DBClient.checkEmail(email);
-    if (checkedEmail) {
-      return res.status(400).send({ error: 'Already exist' });
+    const user = await DBClient.checkEmail(email);
+    if (user) {
+      return res.status(400).json({ error: 'Already exist' });
     }
 
-    try {
-      const hashedPassword = sha1(password);
-      const ops = await DBClient.setNewUser({ email, password: hashedPassword });
-      return res.status(201).send(JSON.stringify({ id: ops.insertedId, email }));
-    } catch (err) {
-      return res.status(500).send({ error: 'internal Error' });
-    }
+    const hashedPassword = sha1(password);
+    const ops = await DBClient.setNewUser({ email, password: hashedPassword });
+    return res.status(201).json({ id: ops.insertedId, email });
   }
 
   static async getMe(req, res) {
