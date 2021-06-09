a71583b57697e6e114a0dc80000fafa76b33550c
mahdi <mahdi.zaabi@gmail.com>
2021-06-09 14:17:15 +0000

    add

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 import auth from '../middleware/auth';
+import preAuth from '../middleware/preAuth';
 
 const router = express.Router();
 /*  Views */
@@ -18,11 +19,11 @@ router.get('/users/me', auth, UsersController.getMe);
 router.post('/files', auth, FilesController.postUpload);
 
 router.get('/files/:id', auth, FilesController.getShow);
-router.get('/files', FilesController.getIndex);
+router.get('/files', auth, FilesController.getIndex);
 
 router.put('/files/:id/publish', auth, FilesController.putPublish);
 router.put('/files/:id/unpublish', auth, FilesController.putUnpublish);
 
-router.get('/files/:id/data', auth, FilesController.getFile);
+router.get('/files/:id/data', preAuth, FilesController.getFile);
 
 export default router;
